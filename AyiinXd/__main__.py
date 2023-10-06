e7beb542c62670091a7c0bf6ec289abda1ed049f
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-06 18:11:57 +0700

    Update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -19,15 +19,15 @@ from traceback import format_exc
 from telethon import version
 from telethon.tl.alltlobjects import LAYER
 
-from AyiinXd import Ayiin, LOGS, LOOP, bot
-from AyiinXd.ayiin import HOSTED_ON, autobot, autopilot, checking, heroku
-from AyiinXd.modules import ALL_MODULES
+from TerlyXd import Csterly, LOGS, LOOP, bot
+from TerluXd.sterly import HOSTED_ON, autobot, autopilot, checking, heroku
+from TerlyXd.modules import ALL_MODULES
 
 
 ON = '''
-❏ ᴀʏɪɪɴ - ᴜsᴇʀʙᴏᴛ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ
+❏ ᴄᴏɴsᴛᴇʀʟʏ - ᴜsᴇʀʙᴏᴛ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ
 ╭╼┅━━━━━╍━━━━━┅╾
-├▹ ᴀʏɪɪɴ ᴠᴇʀsɪᴏɴ : {} •[{}]•
+├▹ ᴄᴏɴsᴛᴇʀʟʏ ᴠᴇʀsɪᴏɴ : {} •[{}]•
 ├▹ ᴜsᴇʀʙᴏᴛ ɪᴅ : {}
 ├▹ ᴜsᴇʀʙᴏᴛ ɴᴀᴍᴇ : {}
 ├▹ ᴀssɪsᴛᴀɴᴛ ɪᴅ : {}
@@ -36,25 +36,25 @@ ON = '''
 '''
 
 
-async def AyiinMain():
+async def CsterlyMain():
     from config import var
 
-    await Ayiin.start()
+    await Csterly.start()
     if not var.BOTLOG_CHATID:
         await autopilot()
     if not var.BOT_TOKEN:
         await autobot()
     try:
         for module_name in ALL_MODULES:
-            imported_module = import_module(f"AyiinXd.modules.{module_name}")
+            imported_module = import_module(f"TerlyXd.modules.{module_name}")
         LOGS.info(f"Python Version - {python_version()}")
         LOGS.info(f"Telethon Version - {version.__version__} [Layer: {LAYER}]")
         LOGS.info(f"Userbot Version - {var.BOT_VER}")
         LOGS.info("[✨ BERHASIL DIAKTIFKAN! ✨]")
-        await checking(Ayiin)
-        me = await Ayiin.get_me()
+        await checking(Csterly)
+        me = await Csterly.get_me()
         bo = await bot.get_me()
-        await Ayiin.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
+        await Csterly.send_message(var.BOTLOG_CHATID, ON.format(var.BOT_VER, HOSTED_ON, me.id, me.first_name, bo.id, bo.first_name))
     except (ConnectionError, KeyboardInterrupt, NotImplementedError, SystemExit):
         pass
     except BaseException as e:
@@ -65,15 +65,15 @@ async def AyiinMain():
 if __name__ == "__main__":
     try:
         heroku()
-        LOOP.run_until_complete(AyiinMain())
+        LOOP.run_until_complete(CsterlyMain())
     except BaseException:
         LOGS.error(format_exc())
         sys.exit()
 
 if len(sys.argv) not in (1, 3, 4):
-    Ayiin.disconnect()
+    Csterly.disconnect()
 else:
     try:
-        Ayiin.run_until_disconnected()
+        Csterly.run_until_disconnected()
     except ConnectionError:
         pass
